6faf4ef46d2bb73e8943ff56e2b13a7c1a80f7bc
jjub0217 <noonssub1004@gmail.com>
2023-11-15 19:53:34 +0900

    classname 통일

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,62 +31,93 @@ $(function () {
     yPercent: -150
   })
 
-  const gnbTl = gsap.timeline({})
-  gnbTl.to($(".gnb"), {
 
+  const sloganText = new SplitType('.section-slogan .title-box p', { types: 'words, chars', });
+  
+  const sloganTl = gsap.timeline({
+    paused: true,
   })
-
-
-
+  sloganTl.from($(".section-slogan picture"), {
+    scale: 1.1
+  })
+  .from($(".section-slogan .title .char"), {
+    opacity: 0,
+    stagger: 0.04,
+    scale:0.5,
+    ease:"elastic.out(1,0.4)",
+  })
+  .from($(".section-slogan .sub-title"), {
+    opacity: 0,
+    yPercent: 100
+  })
   
-  // const sloganTl = gsap.timeline({
-  //   paused: true
-  // })
-  // const sloganText = new SplitType('.section-slogan .title-box p', { types: 'words, chars', });
-  
-  // sloganTl.from($(".section-slogan picture"), {
-  //   scale: 1.1
-  // })
-  // .from($(".section-slogan .title .char"), {
-  //   opacity: 0,
-  //   stagger: 0.04,
-  //   scale:0.5,
-  //   ease:"elastic.out(1,0.4)",
-  // })
-  // .from($(".section-slogan .sub-title"), {
-  //   opacity: 0,
-  //   yPercent: 100
-  // })
 
-
-  $('.gnb .nav-item').hover(function(e){
-      console.log(
-      $(this).find('.sub-list').length)
-
-      if($(this).find('.sub-list').length){
-        $(this).find('.sub-list').addClass('on');
-        $('.gnb').addClass('on')
+  $("[data-scroll]").each(function(i, el) {
+  gsap.to(el, {
+    scrollTrigger: {
+      trigger: el,
+      start: "0% 100%",
+      end: "100% 0%",
+      markers: false,
+      scrub:1,
+      onEnter: function(){
+        $(el).addClass("on")
       }
-  },function(){
-      $(this).find('.sub-list').removeClass('on');
-      $('.gnb').removeClass('on')
+    },
+    yPercent:15
   })
+})
+
+
+$(".section-products .content").each(function(i, el) {
+    prdTl = gsap.timeline({
+      scrollTrigger:{
+        trigger:el,
+        start:"0% 100%",
+        end:"100% 0%",
+        markers:false,
+      },
+    })
+    prdTl
+    .from($(this).find(".background"),.5,{ scale:0, })
+    .from($(this).find('.inner-content'),.5,{ opacity: 0})
+})
 
 
 
-  $('.menu-box').click(function(){
-    $('.gnb').toggleClass('isAct')
-    $('burgerBtn').toggleClass('isAct')
-    $(this).toggleClass('isAct')
-    if ($(".burgerBtn").attr('aria-expanded') === 'false') {
-      $('.burgerBtn').attr('aria-expanded', 'true' )
-    } else {
-      $('.burgerBtn').attr('aria-expanded', 'false' )
+
+$('.gnb .nav-item').hover(function(e){
+    console.log(
+    $(this).find('.sub-list').length)
+    if($(this).find('.sub-list').length){
+      $(this).find('.sub-list').addClass('on');
+      $('.gnb').addClass('on')
     }
-  })
+},function(){
+    $(this).find('.sub-list').removeClass('on');
+    $('.gnb').removeClass('on')
+})
 
+$('.menu-box button').click(function(){
+  $(this).toggleClass('isAct')
+  $('.gnb').toggleClass('isAct')
+  $(this).find('.burgerBtn').toggleClass('isAct')
+  $(this).find(".menu-text").toggleClass("on")
+  if ($(this).attr('aria-expanded') === 'false') {
+    $(this).attr('aria-expanded', 'true' )
+  } else {
+    $(this).attr('aria-expanded', 'false' )
+  }
+})
 
+$('#wave').wavify({
+  height: 60,
+  bones: 3,
+  amplitude: 40,
+  color: '#ff9daf',
+  speed: .25
 
+});
 
   // pc 에서는 손가락으로 슬라이드가 움직여지면 안된다. 그렇기 때문에 옵션을 줘야한다. ===> touchRatio
   const sectionBanner = new Swiper('.section-banner .swiper',{
@@ -106,6 +137,16 @@ $(function () {
   } )
 
 
+  gsap.from($(".section-news .swiper-slide"), {
+    scrollTrigger: {
+      trigger: $(".section-news"),
+      start: "0% 70%",
+      end: "100% 0%",
+      markers: true,
+    },
+    xPercent: 300,
+    stagger: 0.3
+  })
 
   const sectionNews = new Swiper('.section-news .swiper',{
       navigation: {
